bac44e850f1cb16097e9562767f0636d83c1aa5c
Prabesh Narsingh Kunwar <prabeshkunwar12@gmail.com>
2024-05-27 15:52:11 -0300

    basic enums and entities, their properties and relations defined

---
+++ backend/Core/Entities/Candidate.cs
@@ -0,0 +1,16 @@
+﻿namespace backend.Core.Entities
+{
+    public class Candidate
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string CoverLetter { get; set; }
+        public string ResumeUrl { get; set; }
+
+        // Relations
+        public long JobId { get; set; }
+        public Job Job { get; set; }
+    }
+}
